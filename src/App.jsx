63d7f3bc6af89f21fd53b1c4920beacecb49bcc2
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2023-10-13 13:36:02 +0800

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.scss';
 import "./output.css";
 import QueryStrategyPage from "./pages/QueryStrategyPage";
+import DepositWithdrawPage from './pages/DepositWithdrawPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App bg-orange-100 h-full flex
+    <div id="app" className="App bg-orange-100 h-full flex flex-col
       justify-center items-center font-sans">
-      <QueryStrategyPage />
+      <Router>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<QueryStrategyPage />} />
+          <Route path="task3" element={<DepositWithdrawPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/DepositWithdrawPage.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const DepositWithdrawPage = () => {
+    const [depositValue, setDepositValue] = useState(-1);
+
+
+    return (
+        <div id="deposit_withdraw_page">
+
+        </div>
+    )
+}
+
+export default DepositWithdrawPage;
